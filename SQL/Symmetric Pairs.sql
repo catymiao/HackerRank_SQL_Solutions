62f8df4422e7b4c145583c033507405e70efbbc9
catymiao <33770231+catymiao@users.noreply.github.com>
2021-11-10 09:24:54 -0600

    Update Symmetric Pairs.sql

--- SQL/Symmetric Pairs.sql
+++ SQL/Symmetric Pairs.sql
@@ -7,11 +7,10 @@ select f1.x as f1_x, f1.y as f1_y from functions f1
 inner join functions f2
 on f1.x=f2.y
 and f1.y=f2.x)a
-where f1_x <= f1_y
-and  f1_x <> f1_y  
+where f1_x < f1_y
 union (
 select f1.x as f1_x, f1.y as f1_y  from functions f1
-where f1.x=  f1.y
+where f1.x = f1.y
 group by f1_x,f1_y
 having count(*)=2)
 order by f1_x
