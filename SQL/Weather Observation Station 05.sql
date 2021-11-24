a6e1b5be9d0c8be7c688a0b10eb7dd0527f5923f
catymiao <33770231+catymiao@users.noreply.github.com>
2021-11-24 09:11:30 -0600

    Update Weather Observation Station 05.sql

--- SQL/Weather Observation Station 05.sql
+++ SQL/Weather Observation Station 05.sql
@@ -9,3 +9,16 @@ union
 (select city, length(city) 
 from station 
 order by length(city) desc, city asc limit 1)
+
+/*
+Enter your MS SQL Server query here.
+*/
+select * from 
+(select top 1 city, len(city) L
+from station 
+order by len(city) asc , city asc )a 
+union  
+select * from 
+(select top 1 city, len(city) L
+from station 
+order by len(city) desc, city asc )b
